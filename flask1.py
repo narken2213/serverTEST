de499bcd956fc5f34393ee3f6c1e44205b867583
narken2213 <turbopolintags@gmail.com>
2024-02-12 16:55:40 +0300

    first

--- flask1.py
+++ flask1.py
@@ -1,32 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    with open('templates/index.html', 'r', encoding='utf-8') as stream:
-        return stream.read()
-@app.route('/index')
-def index1():
-    with open('templates/index2.html', 'r', encoding='utf-8') as stream:
-        return stream.read()
-@app.route('/promotion')
-def index2():
-    with open('templates/index3.html', 'r', encoding='utf-8') as stream:
-        return stream.read()
-@app.route('/image_mars')
-def index3():
-    with open('templates/index4.html', 'r', encoding='utf-8') as stream:
-        return stream.read()
-@app.route('/promotion_image')
-def index4():
-    with open('templates/index5.html', 'r', encoding='utf-8') as stream:
-        return stream.read()
-@app.route('/astronaut_selection')
-def index5():
-    with open('templates/index6.html', 'r', encoding='utf-8') as stream:
-        return stream.read()
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('base.html', title=title)
 
 
 if __name__ == '__main__':
