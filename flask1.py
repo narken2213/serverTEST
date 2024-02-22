cab866f3b028d40ca17dcbb60567640aab03d0cc
narken2213 <turbopolintags@gmail.com>
2024-02-22 17:21:59 +0300

    alo1

--- flask1.py
+++ flask1.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -12,6 +14,12 @@ def index(title):
 def index1(prof):
     return render_template('trainig.html', prof=prof)
 
+@app.route('/list_prof/<marker>')
+def index2(marker):
+    with open("prof.json", "rt", encoding="utf8") as f:
+        prof_list = json.loads(f.read())
+    print(prof_list)
+    return render_template('news.html', marker=marker, prof_list = prof_list)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=7777)
